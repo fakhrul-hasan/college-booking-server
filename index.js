4e7b617ab2036efc2e9be25eb9fdbf3cd53fe774
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-07-24 18:52:32 +0600

    vercel file added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.xyrtm8p.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +38,11 @@ async function run() {
           res.send(matchingColleges);
         
       });
+      app.get('/collegeDetails/:id', async(req,res)=>{
+        const id = req.params.id;
+        const result = await collegesCollection.find({_id: new ObjectId(id)}).toArray();
+        res.send(result);
+      })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
